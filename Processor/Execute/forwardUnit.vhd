c4047c0bbac4ce12ee44f57a99620b9d03673a6b
MohamedAhmed18 <modyben21@yahoo.com>
2024-04-18 23:23:11 +0200

    Execute finished TEST PLZ YA BEDO

--- Processor/Execute/forwardUnit.vhd
+++ Processor/Execute/forwardUnit.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use IEEE.std_logic_signed.all;
 entity forwardUnit is port(
-    i_rs1,i_rs2,i_rd_ex,i_rs2_ex,i_rd_mem,i_rs2_mem: in std_logic_vector(3 downto 0); --the addresses of the regs
+    i_rs1,i_rs2,i_rd_ex,i_rs2_ex,i_rd_mem,i_rs2_mem: in std_logic_vector(2 downto 0); --the addresses of the regs
     i_wb_ex,i_wb_mem: in std_logic_vector(1 downto 0); -- the writebacks
     o_selector1,o_selector2: out std_logic_vector(2 downto 0)
 );
