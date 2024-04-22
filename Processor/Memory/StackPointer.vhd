672c009e6b03486c4076cdd778f813caa8ccd568
Abdulrahman Elbedewy <96942793+AbdulrahmanEl-Bedewy@users.noreply.github.com>
2024-04-22 22:09:04 +0200

    Update StackPointer.vhd

--- Processor/Memory/StackPointer.vhd
+++ Processor/Memory/StackPointer.vhd
@@ -18,11 +18,13 @@ BEGIN
     BEGIN
         IF i_rst = '1' THEN
             -- initial value of SP is (2^12-1)
-            o_q <= x"00000FFD";
+            o_q <= x"00000FFE";
         ELSIF rising_edge(i_clk) THEN
             IF i_push = '1' THEN
                 o_q <= o_q - 2;
-            ELSIF i_pop = '1' THEN
+            END IF;
+        ELSIF falling_edge(i_clk) THEN
+            IF i_pop = '1' THEN
                 o_q <= o_q + 2;
             END IF;
         END IF;
